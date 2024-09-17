e87bd2d1070d03617a2df2734b33fcfa5a223062
Manoj24G <Gawade.manoj7@gmail.com>
2024-09-17 12:09:11 +0530

    Your loop should now run eight times. Inside the body of the loop, print the value of the i iterator and see what happens.

--- 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
+++ 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
@@ -2,7 +2,9 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-// Step 37
+// Step 39
 for (let i = 0; i < count; i = i + 1) {
-  console.log(i);
+  rows.push(i);
 }
+
+let result = "";
