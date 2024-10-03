035da735a104460f077d604af3e0396c46ebf37d
Manoj24G <Gawade.manoj7@gmail.com>
2024-10-03 07:48:29 +0530

    Step 64

--- 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
+++ 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
@@ -2,7 +2,11 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-// Step 46
+// Step 64
+// Give your padRow function a rowNumber and rowCount parameter. Multiple parameters are separated by a comma:
+
+function padRow(rowNumber, rowCount) {}
+
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1));
 }
