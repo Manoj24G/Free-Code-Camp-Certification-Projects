9b42cc4af912f7c9383d0ec7487086a771e87941
Manoj24G <Gawade.manoj7@gmail.com>
2024-10-10 15:18:23 +0530

    Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.

--- 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
+++ 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
@@ -2,15 +2,15 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-// Step 65
-// Use the return keyword to return the value of the character variable, repeated rowNumber times.
+// Step 66
+// Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
 
 function padRow(rowNumber, rowCount) {
   return character.repeat(rowNumber);
 }
 
 for (let i = 0; i < count; i = i + 1) {
-  rows.push(character.repeat(i + 1));
+  rows.push(padRow());
 }
 
 let result = "";
