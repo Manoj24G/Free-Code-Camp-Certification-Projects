7268a30c5ab4eb7ef9e01b2572d89494ebb2d9bd
Manoj24G <Gawade.manoj7@gmail.com>
2024-11-12 16:29:21 +0530

    Step 43

--- 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
+++ 2. JavaScript Algorithms and Data Structures (Beta) Certification/Section 1/1. Learn Introductory JavaScript by Building a Pyramid Generator/pyramidGenerator.js
@@ -1,8 +1,18 @@
-// Step 39
-// Use let to declare a result variable, and assign it an empty string.
+// Step 43
+// You need to use the special escape sequence \n, which is interpreted as a new line when the string is logged. 
 
 const character = "#";
 const count = 8;
 const rows = [];
 
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
 let result = "";
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
